1a8b8433d3a41faf4d86532a32f181c71fc4361c
Bartosz <bdabkowski@outlook.com>
2016-07-01 15:52:07 -0700

    Nunit version change 3.2.0 to 3.0.10.

---
+++ Interview/Interview/CodeTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace Interview
+{
+    public class CodeTests : AssertionHelper
+    {
+        [Test]
+        [TestCase("HannaH", true)]
+        [TestCase("TrainniarT", true)]
+        [TestCase("Hellej", false)]
+        public void Palindrome(string input, bool result)
+        {
+            Expect(I.IsPalindrome(input), Is.EqualTo(result));
+        }
+
+        [Test]
+        [TestCase(new int[] { 1, 3, 2, 4, 5, 5, 6, 1 }, true)]
+        [TestCase(new int[] { 1, 3, 2, 4, 5, 8, 6, 9 }, false)]
+        public void Duplicate(int[] input, bool result)
+        {
+            Expect(I.IsDuplicate(input), Is.EqualTo(result));
+            Expect(I.IsDuplicate2(input), Is.EqualTo(result));
+        }
+
+        [Test]
+        [TestCase("hello", "olleh")]
+        [TestCase("HowAreYou", "uoYerAwoH")]
+        public void Reverse(string word, string result)
+        {
+            Expect(I.ReverseWord(word), Is.EqualTo(result));
+        }
+    }
+}
---
+++ Interview/Interview/I.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Interview
+{
+    public class I
+    {
+        public static string ReverseWord(string word)
+        {
+            StringBuilder reversedWord = new StringBuilder(word);
+            char temp;
+            for (int i = 0; i < word.Length / 2; i++)
+            {
+                temp = reversedWord[i];
+                reversedWord.Replace(word[i], word[word.Length - 1 - i]);
+                reversedWord.Replace(word[word.Length - 1 - i], temp);
+            }
+
+            return word;
+        }
+        public static bool IsDuplicate(int[] array)
+        {
+            HashSet<int> hashSet = new HashSet<int>();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (hashSet.Contains(array[i]))
+                {
+                    return true;
+                }
+                else
+                {
+                    hashSet.Add(array[i]);
+                }
+            }
+            return false;
+        }
+        public static bool IsDuplicate2(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length - 1; j++)
+                {
+                    if (array[i] == array[j])
+                        return true;
+                }
+            }
+            return false;
+        }
+        public static bool IsPalindrome(string input)
+        {
+            for (int i = 0; i < input.Length / 2; i++)
+            {
+                if (input[i] != input[input.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
